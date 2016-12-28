7153077170828af55c5ffb07a8ae0cfa2ed0a411
Stephane Nicoll <snicoll@pivotal.io>
2016-12-28 10:06:16 +0100

    Improve sample to back-off if JMX isn't enabled

--- src/main/java/com/example/EhCacheJmxConfigurer.java
+++ src/main/java/com/example/EhCacheJmxConfigurer.java
@@ -7,24 +7,31 @@ import javax.management.MBeanServer;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.management.ManagementService;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EhCacheJmxConfigurer {
 
-	private final MBeanServer mBeanServer;
+	private final ObjectProvider<MBeanServer> mBeanServerProvider;
 
 	private final CacheManager cacheManager;
 
-	public EhCacheJmxConfigurer(MBeanServer mBeanServer, CacheManager cacheManager) {
-		this.mBeanServer = mBeanServer;
+	public EhCacheJmxConfigurer(ObjectProvider<MBeanServer> mBeanServerProvider,
+			CacheManager cacheManager) {
+		this.mBeanServerProvider = mBeanServerProvider;
 		this.cacheManager = cacheManager;
 	}
 
 	@PostConstruct
 	public void initializeJmx() {
-		ManagementService.registerMBeans(cacheManager, mBeanServer,
-				true, true, true, true, true);
+		MBeanServer mBeanServer = mBeanServerProvider.getIfAvailable();
+		// You might decide to register on the platform mbean server here or not
+		// even bother to inject the mbean server
+		if (mBeanServer != null) {
+			ManagementService.registerMBeans(cacheManager, mBeanServer,
+					true, true, true, true, true);
+		}
 	}
 
 }
